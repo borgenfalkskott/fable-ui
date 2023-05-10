c0e2e7f6d1ba7203cfce4ca6ab0f9254382ccb98
tommyskott <tommy@borgenfalk.se>
2023-05-10 23:04:23 +0200

    app js: added event listener for DOMContentLoaded event in init

--- src/js/app.js
+++ src/js/app.js
@@ -2,9 +2,10 @@ import Card from "./Card";
 
 const FableUI = {
   init: () => {
-    console.log("fable ui");
-
-    Card.init();
+    window.addEventListener("DOMContentLoaded", (event) => {
+      console.log("fable ui");
+      Card.init();
+    });
   },
 };
 
