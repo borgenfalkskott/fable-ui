eba3eb16c483465bcecd1fdef8f6c9cb785e4d7f
tommyskott <tommy@borgenfalk.se>
2023-05-17 11:15:40 +0200

    js components are now inited after DOMContentLoaded, FableUI obj exported.

--- src/js/fable-ui.js
+++ src/js/fable-ui.js
@@ -3,12 +3,18 @@ import Card from "./Card";
 const FableUI = {
   init: () => {
     console.log("fable ui");
+
+    if (document.readyState === "loading") {
+      document.addEventListener("DOMContentLoaded", FableUI.componentsInit);
+    } else {
+      FableUI.componentsInit();
+    }
+  },
+
+  componentsInit: () => {
+    console.log("components init");
     Card.init();
   },
 };
 
-if (document.readyState === "loading") {
-  document.addEventListener("DOMContentLoaded", FableUI.init);
-} else {
-  FableUI.init();
-}
+export default FableUI;
